14ced7fda6bfb526886e52dc7179da0adb9b5c05
Dhanraj Patil <95074320+Dhanrajpatil123@users.noreply.github.com>
2025-03-18 17:02:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import ImageGallery from './Component/ImageGallery' 
+import CountdownTimer from './Component/CountdownTimer';
 
 
 
@@ -11,6 +12,12 @@ function App() {
 
   return (
     <>
+
+    <marquee><b>BUMPER OFFER...GET 30% OFF ON EACH PRODUCT</b></marquee>
+
+    <div className="w-full max-w-md">
+        <CountdownTimer targetDate="2025-03-19T00:00:00" />
+      </div>
        <ImageGallery></ImageGallery>
     </>
   )
